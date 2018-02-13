ae92f0ae511da81ad29e432a74d10d85d301f04c
Christopher Kuemmel <kuemmel.christopher@gmail.com>
2018-02-13 12:29:30 +0100

    add distance comparison for evaluation

--- tools/evaluate.py
+++ tools/evaluate.py
@@ -1,7 +1,17 @@
 import os
 import json
 import math
+import queue
 
+def distance(aB, bB):
+    # center points
+    aX = aB[0] + (aB[2] / 2)
+    aY = aB[1] + (aB[3] / 2)
+    bX = bB[0] + (bB[2] / 2)
+    bY = bB[1] + (bB[3] / 2)
+    return math.sqrt((bX - aX)**2 + (bY - aY)**2)
+
+# from keras: https://github.com/broadinstitute/keras-rcnn/blob/78313b6e9adf918b92ee1842adc199cb8fbe8df0/keras_rcnn/preprocessing/_object_detection.py#L19-L24
 def union(au, bu, i):
     aA = (max(au[0], au[2]) - min(au[0], au[2])) * (max(au[1], au[3]) - min(au[1], au[3]))
     bA = (max(bu[0], bu[2]) - min(bu[0], bu[2])) * (max(bu[1], bu[3]) - min(bu[1], bu[3]))
@@ -11,7 +21,6 @@ def union(au, bu, i):
     # h = max(au[3], bu[3]) - y
     return aA + bA - i
 
-# from keras: https://github.com/broadinstitute/keras-rcnn/blob/78313b6e9adf918b92ee1842adc199cb8fbe8df0/keras_rcnn/preprocessing/_object_detection.py#L19-L24
 def intersection(ai, bi):
     x = max(ai[0], bi[0])
     y = max(ai[1], bi[1])
@@ -53,23 +62,34 @@ for filename in os.listdir(pred_path):
 
     # eval and pred jsons have same naming?!
     pred_data = json.load(open(pred_path + filename))
-    gold_data = json.load(open(gold_path + filename))
- 
-    for gold_beer in gold_data:
-        gold_beer_coords = (gold_beer['x'], gold_beer['y'], gold_beer['w']+gold_beer['x'], gold_beer['h']+gold_beer['y'])
-        gold_annotations_count += 1
+    gold_data = json.load(open(gold_path + filename))    
+    
+    
+    # if no pred annotation add 0% precision
+    if len(pred_data) == 0:
+        scores.append(0.0)
+    for pred_beer in pred_data:
+        pred_beer_values = (pred_beer['x'], pred_beer['y'], pred_beer['w'], pred_beer['h'])
+        pred_annotations_count += 1
+        q = queue.PriorityQueue()
 
-        # if no pred annotation add 0% precision
-        if len(pred_data) == 0:
-            scores.append(0.0)
+        if len(gold_data) != 0:
+            # compute distance to each gold_beer
+            for gold_beer in gold_data:
+                gold_beer_values = (gold_beer['x'], gold_beer['y'], gold_beer['w'], gold_beer['h'])
+                gold_annotations_count += 1
+                q.put((distance(pred_beer_values, gold_beer_values), gold_beer))
+            
+            # get shortest dist
+            (_, gold_beer) = q.get()
 
-        for pred_beer in pred_data:
             pred_beer_coords = (pred_beer['x'], pred_beer['y'], pred_beer['w']+pred_beer['x'], pred_beer['h']+pred_beer['y'])
-            pred_annotations_count += 1
+            gold_beer_coords = (gold_beer['x'], gold_beer['y'], gold_beer['w']+gold_beer['x'], gold_beer['h']+gold_beer['y'])
 
             # compute intersection over union score
             result = iou(pred_beer_coords, gold_beer_coords)
 
+            # rank scores
             scores.append(result)
             if result == 0.0:
                 no_match.append(result)
@@ -82,14 +102,16 @@ for filename in os.listdir(pred_path):
     bad_match_prec = 0.0
     score_prec = 0.0
 
+    # compute average precisions
     if len(good_match) != 0:
         good_match_prec = sum(good_match)/len(good_match)
     if len(bad_match) != 0:
         bad_match_prec = sum(bad_match)/len(bad_match)
     if len(scores) != 0:
         score_prec = sum(scores)/len(scores)
-        overall_precision.append(sum(scores)/len(scores))
 
+    # sum up precision over all documents
+    overall_precision.append(score_prec)
     eval_data.append({
         'filename': filename[:-5],
         'gold-annotations-count:': len(gold_data),
@@ -103,8 +125,7 @@ for filename in os.listdir(pred_path):
         })
 
 if len(overall_precision) != 0:
-    print('model-precision: ' + str(sum(overall_precision) / len(overall_precision)))
-    
+    print('model-precision: ' + str(sum(overall_precision) / len(overall_precision)))    
     evaluation = {
         'image-evaluation': eval_data,
         'image-count:': len(os.listdir(pred_path)),
